665169aa64e4f0b41173b509dfba4f1929ed0669
Tianyuan Lu <luty14@fudan.edu.cn>
2018-07-21 20:58:19 +1000

    Create onlyinfirst.py

---
+++ onlyinfirst.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(description="""Given tab-separated files""")
+parser.add_argument('cov_files1',
+                    type=argparse.FileType('r'),
+                    help="List of tab-separated files.")
+parser.add_argument('cov_files2',
+                    type=argparse.FileType('r'),
+                    help="List of tab-separated files.")
+args = parser.parse_args()
+
+ref = {}
+tsv = csv.reader(args.cov_files2, delimiter = '\t')
+for line in tsv:
+    scaf = line[0]
+    if scaf not in ref:
+        ref[scaf] = {}
+    pos = line[1]
+    ref[scaf][pos] = 0
+
+tsv = csv.reader(args.cov_files1, delimiter = '\t')
+for line in tsv:
+    if line[0] in ref:
+        if line[1] not in ref[line[0]]:
+            print('\t'.join(line))
+            continue
+    else:
+        print('\t'.join(line))
